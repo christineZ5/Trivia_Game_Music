6e9707266f26e4b1c2136869a863b2b9eb336cd1
christineZ5 <christinetechwork5@gmail.com>
2020-05-06 20:19:28 -0700

    added js

---
+++ assets/javascript/triviaquest.js
@@ -0,0 +1,82 @@
+const sampleResults = [
+    {
+        "category"         : "Music: 80's Music",
+        "type"             : "multiple",
+        "difficulty"       : "hard",
+        "question"         : "What date di MTV makes it's debut?",
+        "correct_answer"   : "August 1, 1981",
+        "incorrect_answers": ["January 1, 1981", "July 4, 1981", "October 31, 1981"]
+    },
+    {
+        "category"         : "Entertainment: Music",
+        "type"             : "multiple",
+        "difficulty"       : "easy",
+        "question"         : "Who was the lead singer of Motley Crue?",
+        "correct_answer"   : "Vince Neil",
+        "incorrect_answers": ["Sammy Hagar", "Jon Bon Jovie", "Dean Cain"]
+    },
+    {
+        "category"         : "Entertainment: Music",
+        "type"             : "multiple",
+        "difficulty"       : "medium",
+        "question"         : "What is the 80's famous MAX HEADROME (the computer reincarnation) name come from _______ ?",
+        "correct_answer"   : "Edison Carter",
+        "incorrect_answers": ["Piston", "Drive shaft", "Crankshaft"]
+    },
+    {
+        "category"         : "Entertainment: Music",
+        "type"             : "multiple",
+        "difficulty"       : "easy",
+        "question"         : "Who was the lead singer of Motley Crue?",
+        "correct_answer"   : "Vince Neil",
+        "incorrect_answers": ["Sammy Hagar", "Jon Bon Jovie", "Dean Cain"]
+    },
+    {
+        "category"         : "Entertainment: Music",
+        "type"             : "multiple",
+        "difficulty"       : "easy",
+        "question"         : "Who was the lead singer of Motley Crue?",
+        "correct_answer"   : "Vince Neil",
+        "incorrect_answers": ["Sammy Hagar", "Jon Bon Jovie", "Dean Cain"]
+    },
+    {
+        "category"         : "Entertainment: Music",
+        "type"             : "multiple",
+        "difficulty"       : "easy",
+        "question"         : "Who was the lead singer of Motley Crue?",
+        "correct_answer"   : "Vince Neil",
+        "incorrect_answers": ["Sammy Hagar", "Jon Bon Jovie", "Dean Cain"]
+    },
+    {
+        "category"         : "Entertainment: Music",
+        "type"             : "multiple",
+        "difficulty"       : "easy",
+        "question"         : "Who was the lead singer of Motley Crue?",
+        "correct_answer"   : "Vince Neil",
+        "incorrect_answers": ["Sammy Hagar", "Jon Bon Jovie", "Dean Cain"]
+    },
+    {
+        "category"         : "Entertainment: Music",
+        "type"             : "multiple",
+        "difficulty"       : "easy",
+        "question"         : "Who was the lead singer of Motley Crue?",
+        "correct_answer"   : "Vince Neil",
+        "incorrect_answers": ["Sammy Hagar", "Jon Bon Jovie", "Dean Cain"]
+    },
+    {
+        "category"         : "Entertainment: Music",
+        "type"             : "multiple",
+        "difficulty"       : "easy",
+        "question"         : "Who was the lead singer of Motley Crue?",
+        "correct_answer"   : "Vince Neil",
+        "incorrect_answers": ["Sammy Hagar", "Jon Bon Jovie", "Dean Cain"]
+    },
+    {
+        "category"         : "Entertainment: Music",
+        "type"             : "multiple",
+        "difficulty"       : "easy",
+        "question"         : "Who was the lead singer of Motley Crue?",
+        "correct_answer"   : "Vince Neil",
+        "incorrect_answers": ["Sammy Hagar", "Jon Bon Jovie", "Dean Cain"]
+    }
+];
